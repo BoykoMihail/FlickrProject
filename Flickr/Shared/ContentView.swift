ca9df90c23845aa55d60b1cbea1de4c14b3f7775
Boyko Mihail <boyko.mihail.al@gmail.com>
2022-06-25 15:27:22 +0300

    initial commit

---
+++ Flickr/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Михаил Бойко on 25.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flickr/Shared/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Shared
+//
+//  Created by Михаил Бойко on 25.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
